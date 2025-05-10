59db340addec08501e7666db87ab53d974ff5f88
Doug Swisher <big.swish@gmail.com>
2025-05-10 12:48:50 -0500

    Player movement

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,11 +1,13 @@
 
 #include "Game.h"
 
+const float Game::PlayerSpeed = 100.f;
+const sf::Time Game::TimePerFrame = sf::seconds(1.f/60.f);
+
+
 Game::Game()
     : m_window(sf::VideoMode({1920u, 1080u}), "SFML Application")
 {
-    m_window.setFramerateLimit(144);
-
     m_player.setRadius(40.f);
     m_player.setPosition({100.f, 100.f});
     m_player.setFillColor(sf::Color::Cyan);
@@ -14,10 +16,21 @@ Game::Game()
 
 void Game::run()
 {
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+
     while (m_window.isOpen())
     {
         processEvents();
-        update();
+
+        timeSinceLastUpdate += clock.restart();
+        while (timeSinceLastUpdate > TimePerFrame)
+        {
+            timeSinceLastUpdate -= TimePerFrame;
+            processEvents();
+            update(TimePerFrame);
+        }
+
         render();
     }
 }
@@ -31,12 +44,47 @@ void Game::processEvents()
         {
             m_window.close();
         }
+        else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+        {
+            if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+            {
+                m_window.close();
+            }
+            else
+            {
+                handlePlayerInput(keyPressed->code, true);
+            }
+        }
+        else if (const auto* keyReleased = event->getIf<sf::Event::KeyReleased>())
+        {
+            handlePlayerInput(keyReleased->code, false);
+        }
     }
 }
 
 
-void Game::update()
+void Game::update(sf::Time deltaTime)
 {
+    sf::Vector2f movement(0.f, 0.f);
+
+    if (m_isMovingUp)
+    {
+        movement.y -= PlayerSpeed;
+    }
+    if (m_isMovingDown)
+    {
+        movement.y += PlayerSpeed;
+    }
+    if (m_isMovingLeft)
+    {
+        movement.x -= PlayerSpeed;
+    }
+    if (m_isMovingRight)
+    {
+        movement.x += PlayerSpeed;
+    }
+
+    m_player.move(movement * deltaTime.asSeconds());
 }
 
 
@@ -45,4 +93,25 @@ void Game::render()
     m_window.clear();
     m_window.draw(m_player);
     m_window.display();
+}
+
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    if (key == sf::Keyboard::Key::W)
+    {
+        m_isMovingUp = isPressed;
+    }
+    else if (key == sf::Keyboard::Key::S)
+    {
+        m_isMovingDown = isPressed;
+    }
+    else if (key == sf::Keyboard::Key::A)
+    {
+        m_isMovingLeft = isPressed;
+    }
+    else if (key == sf::Keyboard::Key::D)
+    {
+        m_isMovingRight = isPressed;
+    }
 }
--- src/Game.h
+++ src/Game.h
@@ -10,12 +10,22 @@ public:
     void run();
 
 private:
+    static const float PlayerSpeed;
+    static const sf::Time TimePerFrame;
+
     void processEvents();
-    void update();
+    void update(sf::Time deltaTime);
     void render();
+
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 
     sf::RenderWindow m_window;
     sf::CircleShape m_player;
+
+    bool m_isMovingUp = false;
+    bool m_isMovingDown = false;
+    bool m_isMovingLeft = false;
+    bool m_isMovingRight = false;
 };
 
 #endif //GAME_H
